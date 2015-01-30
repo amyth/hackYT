363364a0c5b5312b0df3acf3b25bd33c5c232117
Amyth <aroras.official@gmail.com>
2015-01-30 11:53:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+HackYT v.0.1
+============
+
+A simple script to get all video urls of a channel's video page
---
+++ js/hackyt.js
@@ -0,0 +1,11 @@
+
+(function() {
+    window.hackyt.init = function() {
+        alert('Method Called !');
+    };
+
+    // Call the initialize method on script
+    // insertion. If the script is already
+    // been inserted to the DOM, this method
+    // should be called explicitly
+})();
