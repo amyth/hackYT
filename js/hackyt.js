4261824105209578fb8914003abb3c251f46ac7f
Amyth <aroras.official@gmail.com>
2015-01-30 12:53:46 +0530

    Added the code to get vid nums

--- js/hackyt.js
+++ js/hackyt.js
@@ -1,8 +1,42 @@
 
 (function() {
     window.hackyt.init = function() {
-        alert('Method Called !');
+        var lastNum = -1;
+        var videoNum = 0;
+        var videoUrls = [];
+
+        while (videoNum != lastNum) {
+            var x = window.hackyt.clickMoreButton();
+            console.log(x);
+        }
+
     };
+
+    window.hackyt.clickEvent = new MouseEvent("click", {
+        "view": window,
+        "bubbles": true,
+        "cancelable": false
+    });
+
+    window.hackyt.getVidNums = function() {
+        var vids = document.getElementsByClassName('yt-lockup-title');
+        return vids.length;
+    };
+
+    window.hackyt.clickMoreButton = function() {
+        lastNum = videoNum;
+        var loadMB = document.getElementsByClassName('load-more-button');
+        if (loadMB.length > 0) {
+            loadMB = loadMB[0];
+        } else {
+            return false;
+        }
+
+        loadMB.dispatchEvent(window.hackyt.clickEvent);
+        videoNum = window.hackyt.getVidNums();
+
+        return videoNum;
+    }
 
     // Call the initialize method on script
     // insertion. If the script is already
